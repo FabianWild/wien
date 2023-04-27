8e00d9d275e123e23f565669c5d563119c7f3843
FabianWild <Fabian.Wild1996@gmx.de>
2023-04-27 17:42:34 +0200

    bus icons hinzugefügt

--- main.js
+++ main.js
@@ -55,6 +55,16 @@ async function showStops(url){
     L.geoJSON(jsondata).addTo(themaLayer.stops)
     //console.log(response, jsondata)
     L.geoJSON(jsondata, {
+        pointToLayer: function(feature, latlng) {
+            // L.marker(latlng).addTo(map);
+             return L.marker(latlng, {
+                 icon: L.icon({
+                     iconUrl: 'icons/bus.png',
+                     iconAnchor: [16, 37],
+                     popupAnchor: [0, -37],
+                 })
+             });
+         },
         onEachFeature: function(feature, layer){
             let prop = feature.properties;
             layer.bindPopup(`
